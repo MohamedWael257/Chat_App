56fa344d4712fb197dc1f0283e17d9593569d865
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-03-25 16:04:00 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/Chat_App/"
-=======
-//  base: "Chat_App"
->>>>>>> c3a21e62ab10b5d0e8a816af68e8b2afe296c3f5
 })
