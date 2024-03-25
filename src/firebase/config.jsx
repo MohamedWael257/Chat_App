ed631e34b1e311a7a92f9da2f87cfcd001cd9e60
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-03-25 16:56:10 +0200

    first commit

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -8,8 +8,8 @@ const firebaseConfig = {
     projectId: "chatapp-6581b",
     storageBucket: "chatapp-6581b.appspot.com",
     messagingSenderId: "404971613319",
-    appId: "1:404971613319:web:4fdb42ad0b0cff530edc22",
-    measurementId: "G-VXEF9559ZR"
+    appId: "1:404971613319:web:a3b836263d7f7e110edc22",
+    measurementId: "G-EL6ZT7DSQ3"
 };
 
 const app = initializeApp(firebaseConfig);
